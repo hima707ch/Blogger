31d6c3014c8f37324868f1a061ae486600f1734b
hima707ch <107808488+hima707ch@users.noreply.github.com>
2022-06-28 18:05:33 +0530

    up

--- app.js
+++ app.js
@@ -92,10 +92,10 @@ app.post("/profile/:acc",async function(req,res){
     array = account.arr;
     await array.push(post);
     await Profile.updateOne({email:account.email},{$set: { arr:array } }).then((obj) => {
-                    
+                    console.log("updated");
                 })
                 .catch((err) => {
-                  
+                  console.log(err.message);
                 });
     account = await Profile.findOne({ email:account.email });
     
